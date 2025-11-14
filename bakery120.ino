8e3be1745f0eb9ebf9c19a2af8bd65db83be9204
Petint <petras.balint04@gmail.com>
2025-11-14 17:55:56 +0100

    dded bakery120.ino

---
+++ bakery120.ino
@@ -0,0 +1,32 @@
+// Knight Rider type effect
+//
+void setup()
+{ 
+  for (int i = 9; i<14; i++)
+  {pinMode(i, OUTPUT);}
+}
+
+void loop()
+{ 
+  int direction = 1;
+  int currentLed = 9;
+  while (1)
+  {
+    digitalWrite(currentLed, HIGH);
+    if ((currentLed == 13) && (direction = 1))
+    {
+     direction*=-1;
+    }
+    else if ((currentLed == 9) && (direction == -1))
+    {
+      direction*=-1;
+    }
+    else
+    {
+     delay(1);  
+    }
+    digitalWrite(currentLed-direction, LOW);
+      currentLed += direction;
+      delay(200);
+   }
+}
